daadc83caec7eecc3c0c4a7f10175c54854abad7
Quetzalcoatl <www.quetzalcoatl@gmail.com>
2021-02-06 21:12:41 +0300

    add main class and settings

---
+++ alien_invasion/alien_invasion.py
@@ -0,0 +1,30 @@
+import pygame
+import sys
+from settings import Settings
+
+
+class AlienInvasion:
+    def __init__(self):
+        # init module
+        pygame.init()
+        # using external settings as local variable
+        self.settings = Settings()
+        # create a display surface, set it to a class variable
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        while True:
+            # get() returns all last events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            # fill screen with specify color
+            self.screen.fill(self.settings.bg_color)
+            # draw updated screen
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
