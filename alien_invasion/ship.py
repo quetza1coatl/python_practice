af76cf0e98cb49747a95ec23231a051dc2b5367c
Quetzalcoatl <www.quetzalcoatl@gmail.com>
2021-02-08 20:19:27 +0300

    moving for ship

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -10,6 +10,15 @@ class Ship:
         self.rect = self.image.get_rect()
         # New ship is created at the middle-bottom position of the screen
         self.rect.midbottom = self.screen_rect.midbottom
+        # flag for moving in the  -> and <- directions
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
