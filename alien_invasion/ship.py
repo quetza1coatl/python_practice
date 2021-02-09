e652fe51f51e9df2863f02fb8f8e58685ea90b36
Quetzalcoatl <www.quetzalcoatl@gmail.com>
2021-02-09 21:09:22 +0300

    add variable speed

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -3,6 +3,7 @@ import pygame
 
 class Ship:
     def __init__(self, ai_game):
+        self.settings = ai_game.settings
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
         # load picture. Method 'load' returns Surface object!
@@ -13,12 +14,15 @@ class Ship:
         # flag for moving in the  -> and <- directions
         self.moving_right = False
         self.moving_left = False
+        # Rect uses int only, but we need float for more accurate calculations
+        self.x = float(self.rect.x)
 
     def update(self):
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+        self.rect.x = self.x
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
