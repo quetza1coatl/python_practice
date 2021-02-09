b14d9061b12a1a1eb7ddf0696f49c0055bafdb2e
Quetzalcoatl <www.quetzalcoatl@gmail.com>
2021-02-09 22:00:44 +0300

    refactor, add full screen mode

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -12,7 +12,11 @@ class AlienInvasion:
         self.settings = Settings()
 
         # create a display surface, set it to a class variable
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        # pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+
         self.ship = Ship(self)
         pygame.display.set_caption("Alien Invasion")
 
@@ -28,15 +32,23 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
     def _update_screen(self):
         # fill screen with specify color
