53360c300550ab66eed76eb46c8e308a09118309
Quetzalcoatl <www.quetzalcoatl@gmail.com>
2021-02-11 22:33:37 +0300

    add bullet

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -12,7 +12,7 @@ class AlienInvasion:
         self.settings = Settings()
 
         # create a display surface, set it to a class variable
-        # pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        # self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
