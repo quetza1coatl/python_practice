5960f8fa5b6759ec4ca1f382004bc2393a5872ab
Quetzalcoatl <www.quetzalcoatl@gmail.com>
2021-03-06 12:45:32 +0300

    check collision alien-screenbottom

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -89,8 +89,22 @@ class AlienInvasion:
     def _update_aliens(self):
         self._check_fleet_edges()
         self.aliens.update()
+
+        # Look for alien-ship collisions.
         if pygame.sprite.spritecollideany(self.ship, self.aliens):
             self._ship_hit()
+
+        # Look for aliens hitting the bottom of the screen.
+        self._check_aliens_bottom()
+
+    def _check_aliens_bottom(self):
+        """Check if any aliens have reached the bottom of the screen."""
+        screen_rect = self.screen.get_rect()
+        for alien in self.aliens.sprites():
+            if alien.rect.bottom >= screen_rect.bottom:
+                # Treat this the same as if the ship got hit.
+                self._ship_hit()
+                break
 
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
@@ -107,7 +121,6 @@ class AlienInvasion:
 
         # Pause.
         sleep(0.5)
-
 
     def _create_fleet(self):
         alien = Alien(self)
